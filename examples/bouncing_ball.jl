e642f23b2fa54b81742940c78d1c92d4640408ae
belledon <mbelledonne@gmail.com>
2023-08-08 21:03:58 -0400

    added mcmc example

--- examples/bouncing_ball.jl
+++ examples/bouncing_ball.jl
@@ -5,78 +5,7 @@ using PhyBullet
 using Accessors
 using UnicodePlots
 
-
-function simple_scene()
-    client = @pycall pb.connect(pb.DIRECT)::Int64
-    pb.setGravity(0,0,-10; physicsClientId = client)
-
-    # add a table
-    dims = [1.0, 1.0, 0.1] # in meters
-    col_id = pb.createCollisionShape(pb.GEOM_BOX,
-                                     halfExtents = dims,
-                                     physicsClientId = client)
-    obj_id = pb.createMultiBody(baseCollisionShapeIndex = col_id,
-                                basePosition = [0., 0., -0.1],
-                                physicsClientId = client)
-    pb.changeDynamics(obj_id,
-                      -1;
-                      mass = 0., # 0 mass are stationary
-                      restitution = 0.9, # some is necessary
-                      physicsClientId=client)
-
-
-    # add a ball
-    bcol_id = pb.createCollisionShape(pb.GEOM_SPHERE,
-                                      radius = 0.1,
-                                      physicsClientId = client)
-    bobj_id = pb.createMultiBody(baseCollisionShapeIndex = bcol_id,
-                                 basePosition = [0., 0., 1.0],
-                                 physicsClientId = client)
-    pb.changeDynamics(bobj_id,
-                      -1;
-                      mass = 1.0,
-                      physicsClientId=client)
-
-    (client, bobj_id)
-end
-
-@gen function prior(ls::RigidBodyLatents)
-    mass = @trace(uniform(0., 1.), :mass)
-    res = @trace(uniform(0.2, 0.90), :restitution)
-    new_ls = setproperties(ls.data;
-                           mass = mass,
-                           restitution = res)
-    new_latents = RigidBodyLatents(new_ls)
-    return new_latents
-end
-
-@gen function observe(k::RigidBodyState)
-    pos = k.position # XYZ position
-    # add noise to position
-    obs = @trace(broadcasted_normal(pos, 0.01), :position)
-    return obs
-end
-
-@gen function kernel(t::Int, prev_state::BulletState, sim::BulletSim)
-    # use of PhySMC.step
-    next_state::BulletState = PhySMC.step(sim, prev_state)
-    # elem state could be a different type
-    # but here we only have one `RigidBody` element
-    # so  `next_state.kinematics = [RigidBodyState]`
-    obs = @trace(Gen.Map(observe)(next_state.kinematics), :observe)
-    return next_state
-end
-
-@gen function model(t::Int, sim::BulletSim, template::BulletState)
-    # sample new mass and restitution for objects
-    latents = @trace(Gen.Map(prior)(template.latents), :prior)
-    init_state = setproperties(template; latents = latents)
-    # simulate `t` timesteps
-    states = @trace(Gen.Unfold(kernel)(t, init_state, sim), :kernel)
-    return states
-end
-
-@load_generated_functions
+include("../helpers.jl")
 
 function draw_trace(tr::Gen.Trace)
     (t, _, _) = get_args(tr)
---
+++ examples/mcmc.jl
@@ -0,0 +1,112 @@
+using Gen
+using PyCall
+using PhySMC
+using PhyBullet
+using Accessors
+using UnicodePlots
+
+include("__@DIR__/helpers.jl")
+
+@gen function proposal(tr::Gen.Trace)
+    # TODO: fill in TODO's
+    # HINT: https://www.gen.dev/tutorials/iterative-inference/tutorial#mcmc-2
+    #
+    # get previous values from `tr`
+    prev_mass = TODO
+    prev_res = TODO
+    # sample new values conditioned on the old ones
+    mass ~ TODO
+    restitution ~ TODO
+    # the return of this function is not
+    # neccessary but could be useful
+    # for debugging.
+    return (mass, restitution)
+end
+
+"""
+    inference_procedure
+
+Performs Metropolis-Hastings MCMC.
+"""
+function inference_procedure(gm_args::Tuple,
+                             obs::Gen.Choicemap,
+                             steps::Int = 100)
+
+    # start with an initial guess of physical latents
+    # `ls` is the log score or how well this
+    # initial guess explains the observations
+    tr, ls = Gen.generate(model, gm_args, obs)
+
+    println("Initial logscore: $(ls)")
+
+    # TODO: use this to count the number of accepted moves
+    acceptance_count = 0
+    for _ = 1:steps
+        # apply the proposal funciton to generate a
+        # new guess over the ball's latents
+        # that is related to the previous trace
+        # see `?mh` in the REPL for more info
+        tr, accepted = mh(tr, proposal, ())
+
+    end
+
+    acceptance_ratio = 0 # TODO: implement
+
+    println("Final logscore: $(get_score(tr))")
+    println("Acceptance ratio: $(acceptance_ratio)")
+
+    return (tr, acceptance_ratio)
+end
+
+"""
+    data_generating_procedure(t::Int64)
+
+Create a trial (ground truth and observations) with `t` timepoints
+"""
+function data_generating_procedure(t::Int64)
+
+    # start with a ball above a table
+    client, ball_id = simple_scene()
+
+    # configure simulator with the provided
+    # client id
+    sim = BulletSim(;client=client)
+    # This is the object of interest in the scene
+    # (the table is static)
+    ball = RigidBody(ball_id)
+    # Retrieve the default latents for the ball
+    # as well as its initial positions
+    # Note: alternative latents will be suggested by the `prior`
+    init_state = BulletState(sim, [ball])
+    # arguments for `model`
+    gargs = (t, # number of steps
+             sim,
+             init_state)
+
+    # execute `model`
+    trace, _ = Gen.generate(model, gargs)
+    choices = get_choices(trace)
+    # extract noisy positions
+    obs = Gen.choicemap()
+    for i = 1:t
+        addr = :kernel => i => :observe
+        _choices = Gen.get_submap(choices, addr)
+        Gen.set_submap!(obs, addr, _choices)
+    end
+
+    return (gargs, obs)
+
+end
+
+function main()
+
+    t = 60 # 1 second of observations
+    (gargs, obs) = data_generating_procedure(t)
+
+    (tr, aratio) = inference_procedure(gargs, obs)
+
+    return nothing
+end
+
+
+main();
